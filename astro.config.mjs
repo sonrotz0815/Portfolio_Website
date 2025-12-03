687c2aa17a0703bdc8effc53e694dd0499e7d091
Eric König <120642443+Sonrotz0815@users.noreply.github.com>
2025-12-03 19:29:38 +0100

    Changed dist folder for Azure

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  output: 'static',      // important for SWA
+  dist: 'dist',          // default build folder
   integrations: [tailwind()],
 });
